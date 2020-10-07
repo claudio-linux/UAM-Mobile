583129ab3f9cbb929679742d404ed3712c81ce51
Arthuriel <70857035+Arthuriel@users.noreply.github.com>
2020-10-07 20:44:07 -0300

    first

--- Project-mobile/src/app/home/home-routing.module.ts
+++ Project-mobile/src/app/home/home-routing.module.ts
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomePageRoutingModule {"teste123"} 
+export class HomePageRoutingModule {} 
