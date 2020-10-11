0d875eb41f59c617b6181045eebb000cc662edfd
Arthuriel <70857035+Arthuriel@users.noreply.github.com>
2020-10-11 14:34:28 -0300

    Fase 1

--- Project-mobile/src/app/home/home.page.ts
+++ Project-mobile/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  
+  ngOnInit() {
+  }
 
 }
