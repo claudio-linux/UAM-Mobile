953a557d8e7f8fe0ca33a4de0afe5c9681ec1521
Felipe <felipedenani@outlook.com>
2020-10-12 12:09:40 -0300

    ok

--- Project-mobile/src/app/home/home.page.ts
+++ Project-mobile/src/app/home/home.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import React from 'react';
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonRouterOutlet } from '@ionic/react';
+
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  public selecionado = 'exemplo1'; // coloque aqui o default ;)
-  
+public contribuicoes=[
+  {id: 1, name: 'Cerveja', preco:50},
+  {id: 2, name: 'Churrasco', preco:100},
+  {id: 3, name: 'Doce', preco:20}
+]
+
+constructor(){}
 }
