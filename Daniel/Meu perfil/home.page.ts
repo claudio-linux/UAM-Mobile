755284093d0efce89ab5a71da940716035685302
Claudio gabriel <claudiogabriel17@gmail.com>
2020-10-12 17:55:41 -0300

    Colaborators

---
+++ Daniel/Meu perfil/home.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor() {}
+  
+  ngOnInit() {
+  }
+
+}
---
+++ Felipe/Meus eventos/home.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import React from 'react';
+import { IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonRouterOutlet } from '@ionic/react';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+public contribuicoes=[
+  {id: 1, name: 'Cerveja', preco:50},
+  {id: 2, name: 'Churrasco', preco:100},
+  {id: 3, name: 'Doce', preco:20}
+]
+
+constructor(){}
+}
