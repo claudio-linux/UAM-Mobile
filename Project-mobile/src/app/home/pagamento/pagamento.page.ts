bd155ae635e8aed22b2e9c9c7737a5bb33dcacb1
Claudio gabriel <claudio.garcia@mandic.net.br>
2020-10-13 19:23:35 -0300

    novas paginas

---
+++ Project-mobile/src/app/home/pagamento/pagamento.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagamento',
+  templateUrl: './pagamento.page.html',
+  styleUrls: ['./pagamento.page.scss'],
+})
+export class PagamentoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
