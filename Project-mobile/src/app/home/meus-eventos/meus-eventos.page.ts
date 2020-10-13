d8c29ef8b4ec747945730a9df8477ba5aec878a0
Claudio gabriel <claudiogabriel17@gmail.com>
2020-10-12 23:24:00 -0300

    Pagina Meus eventos adicionada

--- Project-mobile/src/app/home/meus-eventos/meus-eventos.page.ts
+++ Project-mobile/src/app/home/meus-eventos/meus-eventos.page.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meus-eventos.page.scss'],
 })
 export class MeusEventosPage implements OnInit {
-
+  
+  public contribuicoes=[
+    {id: 1, name: 'Cerveja', preco:50},
+    {id: 2, name: 'Churrasco', preco:100},
+    {id: 3, name: 'Doce', preco:20}
+  ]
   constructor() { }
 
   ngOnInit() {
