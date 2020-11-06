12e00ce3f9805f6e837990df20e4c6eb0f2c9fb9
Claudio gabriel <claudio.garcia@mandic.net.br>
2020-11-05 21:06:46 -0300

    teste

--- Project-mobile/src/app/app.module.ts
+++ Project-mobile/src/app/app.module.ts
@@ -8,11 +8,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+<<<<<<< HEAD
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot()
+  ],
+=======
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    IonicStorageModule.forRoot()],
+>>>>>>> origin/Master
   providers: [
     StatusBar,
     SplashScreen,
