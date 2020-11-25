5c746ae234fafc334e947163eb3266f8c936fbdc
Bruno Bazzo <bazzo.bruno@live.com>
2020-11-24 21:35:25 -0300

    armazenamento offline Lista de Amigos parte 1

---
+++ Project-mobile/src/app/home/lista-de-amigos/home.page.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Item, ItemService } from '../service/itens.service';
+import { debounceTime, map, tap } from 'rxjs/operators';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { DetalhesItemComponent } from './detalhes-item/detalhes-item.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  private search$ = new BehaviorSubject('');
+  public itens: Observable<Item[]> =
+    combineLatest([this.itemService.all(), this.search$]).pipe(
+      debounceTime(200),
+      map(([is, str]) => is.filter(i => i.name.startsWith(str))),
+      map(is => [...is].sort((a, b) => a.name.localeCompare(b.name))),
+    )
+
+  constructor(
+    private modalController: ModalController,
+    private itemService: ItemService
+  ) { }
+
+  public updateSearch(str: string) {
+    this.search$.next(str);
+  }
+
+  async showDetails(itens: Listadeamigos) {
+    const modal = await this.modalController.create({
+      component: DetalhesItemComponent,
+      componentProps: {
+        itens
+      }
+    });
+
+    await modal.present();
+  }
+
+  public doRefresh(event) {
+    this.itemService.add();
+    event.target.complete();
+  }
+
+}
