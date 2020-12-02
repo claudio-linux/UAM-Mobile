aaea2cc28c5e707be6f960b4c6bc41355b924f2b
Claudio gabriel <claudio.garcia@mandic.net.br>
2020-12-02 20:20:29 -0300

    update

--- Project-mobile/src/app/home/lista-de-amigos/shared/shared.module.ts
+++ Project-mobile/src/app/home/lista-de-amigos/shared/shared.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ItemFormComponent } from '../../evento-ativo/shared/components/item-form/item-form.component';
-// import { AmigoFormComponent } from './components/amigo-form/amigo-form.component';
+import { AmigoFormComponent } from './components/amigo-form/amigo-form.component';
 
 
 @NgModule({
   declarations: [ItemFormComponent],
-  exports: [ItemFormComponent],
+  exports: [AmigoFormComponent, ItemFormComponent],
   imports: [
     CommonModule,
     FormsModule,
